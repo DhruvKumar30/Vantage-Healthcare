dfdfc834a1f9fc1b2b91ef92114f7bbb1791f70f
Dhruv Kumar <dhruvka2030@gmail.com>
2026-05-10 13:36:03 -0700

    Initial Commit

---
+++ Vantage Healthcare/Vantage_HealthcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vantage_HealthcareApp.swift
+//  Vantage Healthcare
+//
+//  Created by Dhruv Kumar on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Vantage_HealthcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
